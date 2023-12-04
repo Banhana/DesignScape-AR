6b8015cb2814423165d4f5b6be4432709bfc989b
Tony Banh <T.Banh@ymail.com>
2023-12-04 14:51:10 -0800

    Added Firebase Database

--- DesignScape/DesignScape/DesignScapeApp.swift
+++ DesignScape/DesignScape/DesignScapeApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+// Firebase Stuff
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+    }
+}
 
 @main
 struct DesignScapeApp: App {
+    // Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
