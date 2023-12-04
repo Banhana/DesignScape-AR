c00a69ef7509c610c660e6538eaaeb2d0f1e9db5
Tony Banh <T.Banh@ymail.com>
2023-12-04 14:48:23 -0800

    Added Firebase

---
+++ DesignScape/DesignScape/DesignScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignScapeApp.swift
+//  DesignScape
+//
+//  Created by Tony Banh on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
