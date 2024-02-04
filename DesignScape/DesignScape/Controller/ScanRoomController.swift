6ff4d859fc8dd7197852cd67e21e8b0a504008e7
Marvin Huynh <huynhtuhoangminh@gmail.com>
2024-02-03 20:04:27 -0800

    add scan room view

---
+++ DesignScape/DesignScape/Controller/ScanRoomController.swift
@@ -0,0 +1,20 @@
+//
+//  ScanRoomController.swift
+//  DesignScape
+//
+//  Created by Minh Huynh on 2/1/24.
+//
+
+import SwiftUI
+
+struct ScanRoomController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScanRoomController_Previews: PreviewProvider {
+    static var previews: some View {
+        ScanRoomController()
+    }
+}
