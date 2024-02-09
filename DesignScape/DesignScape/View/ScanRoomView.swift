a70e2721fe2a5396fb3217e6eba9d96db4774593
Marvin Huynh <huynhtuhoangminh@gmail.com>
2024-02-09 00:10:44 -0800

    implement scanning a room with RoomPlan api obtain a 3D model from the scan redesign CreateDesignView to contain 2 modes

---
+++ DesignScape/DesignScape/View/ScanRoomView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanRoomView.swift
+//  DesignScape
+//
+//  Created by Minh Huynh on 2/8/24.
+//
+
+import SwiftUI
+
+struct ScanRoomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanRoomView()
+}
